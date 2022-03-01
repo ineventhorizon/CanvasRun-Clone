e81a7f9e918e16909f9de67c86953e2968b9d290
Yusuf Ekrem Keçilioğlu <yekecilioglu@gmail.com>
2022-03-01 21:18:42 +0300

    first commit

---
+++ Assets/Game/Scripts/UI/ProgressBar.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode()]
+public class ProgressBar : MonoBehaviour
+{
+    [SerializeField] private int maximum;
+    [SerializeField] private Image imageToFill;
+    public void SetCurrentFill(float current)
+    {
+        float fillAmount = ((float)current / (float)maximum);
+        imageToFill.fillAmount = fillAmount;
+    }
+}
